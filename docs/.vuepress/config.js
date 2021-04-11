586c23071a98437bae54e35211de6578f0ea3d8a
moonyellow <moonyellow@126.com>
2021-04-11 19:36:12 +0800

    20210411 加入gitignore

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -49,6 +49,20 @@ module.exports = {
         sidebarDepth: 1,
         children: [
           {
+            title: 'base',
+            path: '/css/base/',
+            collapsable: false,
+            sidebarDepth: 2,
+            children: [
+              {
+                title: 'em',
+                path: '/css/base/dimensions/',
+                collapsable: false,
+                sidebarDepth: 3
+              }
+            ]
+          },
+          {
             title: 'font',
             path: '/css/font/',
             collapsable: false,
@@ -63,14 +77,14 @@ module.exports = {
             ]
           },
           {
-            title: 'base',
-            path: '/css/base/',
+            title: 'less',
+            path: '/css/less',
             collapsable: false,
             sidebarDepth: 2,
             children: [
               {
-                title: 'em',
-                path: '/css/base/dimensions/',
+                title: 'other',
+                path: '/css/less/other/',
                 collapsable: false,
                 sidebarDepth: 3
               }
@@ -167,10 +181,16 @@ module.exports = {
         sidebarDepth: 1,    // 可选的, 默认值是 1
       },
       {
+        title: 'EN',   // 必要的
+        path: '/en/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+      },
+      {
         title: 'Group 2',
         children: [ /* ... */],
         initialOpenGroupIndex: -1 // 可选的, 默认值是 0
-      }
+      },
     ],
     lastUpdated: 'Last Updated', // string | boolean
   },
---
+++ docs/en/README.md
@@ -0,0 +1,33 @@
+## en
+
+### make no sense to
+
+make no sense to do sth
+做什么没有意义
+
+### suffice
+英[səˈfaɪs] 美[səˈfaɪs]
+v.	足够; 足以;
+
+### gorgeous	
+英[ˈɡɔːdʒəs] 美[ˈɡɔːrdʒəs]
+adj.	非常漂亮的; 美丽动人的; 令人愉快的; 绚丽的; 灿烂的; 华丽的;
+
+### tweaking	
+英[ˈtwiːkɪŋ]
+美[ˈtwiːkɪŋ]
+v.	扭; 拧; 扯; 稍稍调整(机器、系统等);
+[词典]	tweak的现在分词;
+
+
+whirl	英[wɜːl]
+美[wɜːrl]
+v.	(使)旋转，回旋，打转; (头脑、思想等)混乱不清，激动，恍惚;
+n.	旋转; 回旋; 急转; 接连不断的活动; 纷至沓来的事件;
+
+encapsulated
+英[ɪnˈkæpsjuleɪtɪd]
+美[ɪnˈkæpsjuleɪtɪd]
+v.	简述; 概括; 压缩;
+[词典]	encapsulate的过去分词和过去式;
+keep components encapsulated 保持组件的封装性
